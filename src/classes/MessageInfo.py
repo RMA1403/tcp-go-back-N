f00a08e5b8ef9eb991d03cd5b0d4f60b37cc3e6d
Rava Maulana <ravamaulana14@gmail.com>
2023-11-22 15:33:21 +0700

    feat: removed messageInfo class

--- src/classes/MessageInfo.py
+++
@@ -1,7 +0,0 @@
-from Segment import Segment
-
-class MessageInfo:
-    def __init__(self, ip: str, port: int, segment: Segment) -> None:
-        self.ip = ip
-        self.port = port
-        self.segment = segment
