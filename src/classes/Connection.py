bd1ba2dd9508db9e0ce9574f6c99fb08ebd603b4
Rava Maulana <ravamaulana14@gmail.com>
2023-11-22 15:31:51 +0700

    feat: added connection class

--- src/classes/Connection.py
+++ src/classes/Connection.py
@@ -1,27 +1,22 @@
-from socket import socket
-from typing import Callable
+import socket
 
-from classes.MessageInfo import MessageInfo
+from .Segment import Segment
+
 
 class Connection:
-    def __init__(self, ip: str, port: int, socket: socket):
+    def __init__(self, ip: str, port: int):
         self.ip = ip
         self.port = port
-        self.socket = socket
 
-        self.handler: Callable[[MessageInfo], None] = None
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.sock.bind((ip, port))
 
-    def send(self, remote_ip: str, remote_port: int):
-        self.socket.send()
+    def send(self, remote_ip: str, remote_port: str, data: Segment):
+        self.sock.sendto(data, (remote_ip, remote_port))
 
-    def listen(self):
-        pass
+    def listen(self, timeout):
+        self.sock.settimeout(timeout)
+        return self.sock.recvfrom(32768)
 
     def close(self):
-        pass
-
-    def register_handler(self, hanlder: Callable[[MessageInfo], None]):
-        self.handler = hanlder
-
-    def notify(self):
-        pass
+        self.sock.close()
